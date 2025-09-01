17a575d41d0f88ccf215e013a8ed93b0856ecab5
Wycliff Kimutai <99463181+kimutaiwycliff@users.noreply.github.com.>
2025-09-01 23:07:40 +0300

    some styles changes

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -31,7 +31,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
 
   return (
     <header
-      className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60   "
+      className="sticky top-0 z-50 w-full border-b  bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60   "
       {...(theme ? { 'data-theme': theme } : {})}
     >
       <div className="container flex h-16 max-w-screen-2xl items-center justify-between px-4 sm:px-6 lg:px-8">
